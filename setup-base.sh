56c846a0d2085dafb8d69c2d8f56b16bbc19c4b9
as3ii <as3ii777@gmail.com>
2021-06-09 18:58:08 +0200

    major refactor and update

--- setup-base.sh
+++ setup-base.sh
@@ -109,27 +109,6 @@ while [ -n "$1" ]; do
 done
 
 
-### Device selection
-# print device list
-print_info "Device list: $(find /dev/ -regex "/dev/\(sd[a-z]\|nvme[0-9]n[0-9]\)")\n"
-# loop as long as $device is a valid device
-while [ -z "$device" ] || [ ! -e "$device" ] || \
-    ! expr "$device" : '^/dev/\(sd[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; do
-    print_info "Type the device name ('/dev/' required): "
-    read -r device
-    [ ! -e "$device" ] && print_error "This device doesn't exist\n"
-    if ! expr "$device" : '^/dev/\(sd[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; then
-        print_error "You should type a device name, not a partition name\n"
-        device=""
-    fi
-done
-# check disk and ask if it is correct
-sgdisk -p "$device"
-print_info "This device will be wiped, are you sure you want to use this device? [y/N] "
-read -r sure
-[ "$sure" != 'y' ] && exit
-
-
 ### load keyboard layout
 while [ -z "$keyboard" ] || ! localectl list-keymaps | grep -q "^$keyboard$"; do
     print_info "Type the keymap code (es. en): "
@@ -138,7 +117,6 @@ done
 loadkeys "$keyboard"
 print_ok "Keymap loaded\n"
 
-set -eu
 
 ### check efi/bios
 if [ -d /sys/firmware/efi/efivars ]; then
@@ -150,113 +128,146 @@ else
 fi
 
 
-### check ssh/hdd
-if [ "$(cat "/sys/block/$(echo "$device" | sed 's/\/dev\///')/queue/rotational")" -eq "0" ]; then
-    ssd=true
-    print_ok "SSD detected\n"
-else
-    ssd=false
-    print_ok "HDD detected\n"
-fi
+# case with both variable empty
+if [ -z "$boot_path" ] && [ -z "$root_path" ]; then
+    root_path="/mnt"
+    boot_path="$root_path/boot"
+    efi_path="$boot_path"
+    ### Device selection
+    # print device list
+    print_info "Device list: $(find /dev/ -regex "/dev/\(sd[a-z]\|nvme[0-9]n[0-9]\)")\n"
+    # loop as long as $device is a valid device
+    while [ -z "$device" ] || [ ! -e "$device" ] || \
+        ! expr "$device" : '^/dev/\(sd[a-z]\|vd[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; do
+        print_info "Type the device name ('/dev/' required): "
+        read -r device
+        [ ! -e "$device" ] && print_error "This device doesn't exist\n"
+        if ! expr "$device" : '^/dev/\(sd[a-z]\|vd[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; then
+            print_error "You should type a device name, not a partition name\n"
+            device=""
+        fi
+    done
 
+    # check disk and ask if it is correct
+    sgdisk -p "$device"
+    print_info "This device will be wiped, are you sure you want to use this device? [y/N] "
+    read -r sure
+    [ "$sure" != 'y' ] && exit
 
-### partitioning
-#  260MiB EFI
-#  remaining: Linux Filesystem
-print_info "Partitioning $device\n"
-sgdisk --zap-all "$device"
-if $efi; then
-    sgdisk -n 0:0:+260MiB -t 0:ef00 -c 0:BOOT "$device"
-    boot="${device}1"
-    root_dev="${device}2"
-else
-    sgdisk -n 0:0:+1MiB -t 0:ef02 "$device"
-    sgdisk -n 0:0:+259MiB -t 0:8304 -c 0:BOOT "$device"
-    boot="${device}2"
-    root_dev="${device}3"
-fi
-if $crypt; then
-    sgdisk -n 0:0:0 -t 0:8309 -c 0:cryptroot "$device"
-else
-    sgdisk -n 0:0:0 -t 0:8304 -c 0:root "$device"
-fi
-
-
-# force re-reading the partition table
-sync
-partprobe "$device"
-
-# print results
-sgdisk -p "$device"
-
-### preparing partitions
-# boot
-print_info "Formatting boot partition\n"
-mkfs.vfat "$boot" # BOOT partition
-
-# crypt root partition
-if $crypt; then
-    print_info "Setting luks2 partition\n"
-    cryptsetup --type luks2 luksFormat "$root_dev"
-    cryptsetup open "$root_dev" cryptroot
-    root="/dev/mapper/cryptroot"
-else
-    root="$root_dev"
-fi
-
-# formatting root partition using btrfs
-print_info "Formatting root in btrfs\n"
-mkfs.btrfs -L arch --checksum xxhash "$root"
-
-# common mount options
-mntopt="autodefrag,space_cache=v2,noatime,compress=zstd:2"
-mntopt_nocow="autodefrag,space_cache=v2,noatime,nocow"
-if $ssd; then
-    mntopt="$mntopt,discard=async"
-    mntopt_nocow="$mntopt,discard=async"
-fi
-
-# mount the new btrfs partition with some options
-mount -o "$mntopt" "$root" /mnt
-
-# subvolume array
-subvolumes="@ @snapshot @home @opt @root @usr_local @var_log"
-subvolumes_nocow="@swap @tmp @var_cache @var_tmp"
-
-# create root, swap and snapshot subvolumes
-print_info "Creating subvolumes\n"
-for sv in $subvolumes; do
-    btrfs subvolume create "/mnt/$sv"
-done
-for sv in $subvolumes_nocow; do
-    btrfs subvolume create "/mnt/$sv"
-done
-sync
-umount /mnt
-
-print_info "Mounting subvolumes\n"
-# mount subvolumes
-for sv in $subvolumes; do
-    dir="/mnt/$(echo "${sv#@}" | sed 's/_/\//g')"
-    if [ "$sv" != "@" ]; then
-        mkdir -p "$dir"
+    ### partitioning
+    #  260MiB EFI
+    #  remaining: Linux Filesystem
+    print_info "Partitioning $device\n"
+    sgdisk --zap-all "$device"
+    if $efi; then
+        sgdisk -n 0:0:+260MiB -t 0:ef00 -c 0:BOOT "$device"
+        boot="${device}1"
+        root_dev="${device}2"
+    else
+        sgdisk -n 0:0:+1MiB -t 0:ef02 "$device"
+        sgdisk -n 0:0:+259MiB -t 0:8304 -c 0:BOOT "$device"
+        boot="${device}2"
+        root_dev="${device}3"
     fi
-    mount -o "$mntopt,subvol=$sv" "$root" "$dir"
-done
-
-# mount subvolumes with nocow
-for sv in $subvolumes_nocow; do
-    dir="/mnt/$(echo "${sv#@}" | sed 's/_/\//g')"
-    if [ "$sv" != "@" ]; then
-        mkdir -p "$dir"
+    if $crypt; then
+        sgdisk -n 0:0:0 -t 0:8309 -c 0:cryptroot "$device"
+    else
+        sgdisk -n 0:0:0 -t 0:8304 -c 0:root "$device"
     fi
-    mount -o "$mntopt_nocow,subvol=$sv" "$root" "$dir"
-    chattr +C -R "$dir"
-done
 
-# mount boot/EFI partition
-mkdir /mnt/boot
-mount "$boot" /mnt/boot
+
+    # force re-reading the partition table
+    sync
+    partprobe "$device"
+
+    # print results
+    sgdisk -p "$device"
+
+    ### preparing partitions
+    # boot
+    print_info "Formatting boot partition\n"
+    mkfs.vfat "$boot" # BOOT partition
+
+    # crypt root partition
+    if $crypt; then
+        print_info "Setting luks2 partition\n"
+        cryptsetup --type luks2 luksFormat "$root_dev"
+        cryptsetup open "$root_dev" cryptroot
+        root="/dev/mapper/cryptroot"
+    else
+        root="$root_dev"
+    fi
+
+    # formatting root partition using btrfs
+    print_info "Formatting root in btrfs\n"
+    mkfs.btrfs -L arch --checksum xxhash "$root"
+
+
+    ### check ssh/hdd
+    if [ "$(cat "/sys/block/$(echo "$device" | sed 's/\/dev\///')/queue/rotational")" -eq "0" ]; then
+        ssd=true
+        print_ok "SSD detected\n"
+    else
+        ssd=false
+        print_ok "HDD detected\n"
+    fi
+
+    # common mount options
+    mntopt="autodefrag,space_cache=v2,noatime,compress=zstd:2"
+    mntopt_nocow="autodefrag,space_cache=v2,noatime,nocow"
+    if $ssd; then
+        mntopt="$mntopt,discard=async"
+        mntopt_nocow="$mntopt,discard=async"
+    fi
+
+    # mount the new btrfs partition with some options
+    mount -o "$mntopt" "$root" "$root_path"
+
+    # subvolume array
+    subvolumes="@ @snapshot @home @opt @root @usr_local @var_log"
+    subvolumes_nocow="@swap @tmp @var_cache @var_tmp"
+
+    # create root, swap and snapshot subvolumes
+    print_info "Creating subvolumes\n"
+    for sv in $subvolumes; do
+        btrfs subvolume create "$root_path/$sv"
+    done
+    for sv in $subvolumes_nocow; do
+        btrfs subvolume create "$root_path/$sv"
+    done
+    sync
+    umount "$root_path"
+
+    print_info "Mounting subvolumes\n"
+    # mount subvolumes
+    for sv in $subvolumes; do
+        dir="$root_path/$(echo "${sv#@}" | sed 's/_/\//g')"
+        if [ "$sv" != "@" ]; then
+            mkdir -p "$dir"
+        fi
+        mount -o "$mntopt,subvol=$sv" "$root" "$dir"
+    done
+
+    # mount subvolumes with nocow
+    for sv in $subvolumes_nocow; do
+        dir="$root_path/$(echo "${sv#@}" | sed 's/_/\//g')"
+        if [ "$sv" != "@" ]; then
+            mkdir -p "$dir"
+        fi
+        mount -o "$mntopt_nocow,subvol=$sv" "$root" "$dir"
+        chattr +C -R "$dir"
+    done
+
+    # mount boot/EFI partition
+    mkdir "$boot_path"
+    mount "$boot" "$boot_path"
+elif [ -z "$boot_path" ] || [ -z "$root_path" ]; then
+    print_error "\`-b\` and \`-r\` must be set at the same time\n"
+    exit 1
+fi
+if [ -z "$efi_path" ]; then
+    efi_path="$boot_path"
+fi
 
 
 # start installation script
@@ -271,7 +282,8 @@ if [ "$os_id" = "arch" ]; then
             https://raw.githubusercontent.com/as3ii/as3ii-installer/master/arch.sh
         chmod u+x arch.sh
     fi
-    ./arch.sh "$flag_c" -k "$keyboard" -d "$device" -r "$root_dev"
+    ./arch.sh "$flag_c" -k "$keyboard" -d "$device" -r "$root_dev" \
+        -b "$boot_path" -r "$root_path" -e "$efi_path"
 elif [ "$os_id" = "nixos" ]; then
     if ! [ -e ./nixos.sh ]; then
         curl --proto '=https' -sSfO \
